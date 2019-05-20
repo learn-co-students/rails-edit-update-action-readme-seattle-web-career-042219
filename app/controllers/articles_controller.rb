e4e82a7538192b3e5e5da30c28037457347b16ae
Danny Tseng <github email address>
2019-05-19 22:21:56 -0700

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,4 +20,19 @@ class ArticlesController < ApplicationController
   end
 
   # add edit and update methods here
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(post_params)
+    redirect_to article_path(@article)
+  end
+
+  private
+
+  def post_params
+    params.require(:article).permit(:title, :description)
+  end
 end
